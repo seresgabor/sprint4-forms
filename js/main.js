4c759a68ce053cb7e2bb15c043ca9373ba59cdbd
Me <thesis.seres@gmail.com>
2020-06-17 01:39:22 +0200

    sprint5-6

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
-    let total = parseInt(amountInput.value) * price;
-    showAmount.innerHTML = total;
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    
+    showTotalPrice(price, amountNumber);
 }
+
+function showTotalPrice(price=1000, amountNumber=1) {
+    let showAmount = document.querySelector("span.show-amount");
+    if (amountNumber > 10) {
+        alert ("Maximum 10 terméket vásárolhat!");
+    } else if ( amountNumber < 1 ) {
+        alert ("Minimum 1 terméket kell vásárolnia!");
+    } else {
+        let total = amountNumber * price;
+        showAmount.innerHTML = total;
+    }
+}
+
 
